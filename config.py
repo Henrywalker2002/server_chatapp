99e11b748e2bb5288977cbd311dff94d68af301f
Henrywalker2002 <46373762+Henrywalker2002@users.noreply.github.com>
2022-11-12 16:54:47 +0700

    config db

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from app import app
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'chatapp'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'b169ca9901c596'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'b2e46d37'
+app.config['MYSQL_DATABASE_DB'] = 'heroku_35e1273324ea10d'
+app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-06.cleardb.net'
 mysql.init_app(app)
