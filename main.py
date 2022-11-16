fb53d128586c3027bd85362f525ec3f776ec28f1
Henrywalker2002 <46373762+Henrywalker2002@users.noreply.github.com>
2022-11-16 19:40:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ from app import app
 from config import mysql
 from flask import jsonify
 from flask import flash, request
-
-@app.route('/')
-def index():
-    return "deploy success"
+from tkinter import *
 
 @app.route('/acc', methods = ['GET'])
 def signin():
@@ -15,37 +12,19 @@ def signin():
         json_ = request.json
         username = json_['username']
         password = json_['password']
-        IP = json_['IP']
-        port = json_['port']
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        rc = cursor.execute("SELECT * from account WHERE username = %s and pass = %s", (username, password))
+        rc = cursor.execute("SELECT * from account WHERE username = %s", (username))
+        res = cursor.fetchall()
         if rc == 0:
-            return jsonify("not ok")
-        rc = cursor.execute("UPDATE account set IP = %s, Port = %s, STATUS = %s WHERE username = %s", (IP, port, "on", username))
+            cursor.execute("insert into account(username,pass, status) values (%s, %s, %s)", (username, password, "on"))
+            d = {"username":username, "pass": password, "STATUS":"on", "ID": ""}
+            return jsonify(d)
+        if password != res[0]['pass']:
+            return jsonify("wrong pass")
+        cursor.execute("update account set isOnl = 1 where username = %s" , username)
+        d = {"username":username, "pass": password, "isOnl": 1 , "ID": res[0]['ID']}
         conn.commit()
-        return jsonify("OK")
-    except Exception as e:
-        print(e)
-    finally:
-        if conn.open:
-            cursor.close()
-            conn.close()
-
-@app.route('/acc', methods = ['POST'])
-def signup():
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        json_ = request.json
-        name = json_['name']
-        username = json_['username']
-        password = json_['password']
-        rc = cursor.execute("INSERT INTO account (name, username, pass) values (%s, %s,%s)", (name,username, password))
-        conn.commit()
-        if rc == 0:
-            reponse = jsonify("not ok")
-            return reponse
-        return jsonify("OK")
+        return jsonify(d)
     except Exception as e:
         print(e)
     finally:
@@ -60,29 +39,10 @@ def setOff():
         cursor = conn.cursor(pymysql.cursors.DictCursor)
         json_ = request.json
         username = json_['username']
-        rc = cursor.execute('UPDATE account set STATUS = %s WHERE username = %s', ("off", username))
+        rc = cursor.execute('update account set isOnl = 0 where username = %s', username)
         if rc == 0:
             return jsonify("not ok")
         return jsonify("OK")
-    except Exception as e:
-        print(e)
-    finally:
-        if conn.open:
-            cursor.close()
-            conn.close()
-
-@app.route('/getIP', methods = ['GET'])
-def getIP():
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        json_ = request.json
-        username = json_['username']
-        rc = cursor.execute("SELECT IP, port FROM account WHERE username = %s", username)
-        if rc == 0:
-            return jsonify("not ok")
-        res = cursor.fetchall()
-        return jsonify(res)
     except Exception as e:
         print(e)
     finally:
