f65b9dd62fae7fefdb1f0de65922652909c09f89
Henrywalker2002 <46373762+Henrywalker2002@users.noreply.github.com>
2022-11-26 10:11:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,85 +19,42 @@ def signin():
         rc = cursor.execute("SELECT * from account WHERE username = %s", (username))
         res = cursor.fetchall()
         if rc == 0:
-            cursor.execute("insert into account(username,pass, isOnl) values (%s, %s, %s)", (username, password, "on"))
-            conn.commit()
-            d = {"username":username, "pass": password, "isOnl": 1, "ID": ""}
-            return jsonify(d)
+            d = {"result":"fail", "message":"no username found"}
         if password != res[0]['pass']:
             d = {"result":"fail", "message":"wrong password"}
-            return jsonify(d)
-        cursor.execute("update account set isOnl = 1 where username = %s" , username)
-        d = {"username":username, "pass": password, "isOnl": 1 , "ID": res[0]['ID'], "result":"ok"}
-        conn.commit()
+            return jsonify(d)     
+        d = {"username":username, "result":"ok"}
         return jsonify(d)
     except Exception as e:
-        print(e)
+        return jsonify(e)
     finally:
         if conn.open:
             cursor.close()
             conn.close()
             
-@app.route('/acc', methods = ['PUT'])
-def setOff():
+@app.route('/signUp', methods= ['GET', 'POST'])
+def createAcc():
     try:
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
         json_ = request.json
         username = json_['username']
-        rc = cursor.execute('update account set isOnl = 0 where username = %s', username)
-        if rc == 0:
-            d = {"result":"fail"}
+        password = json_['password']
+        name = json_['name']
+        email = json_['email']
+        rc = cursor.execute("select * from account where username = %s or email = %s", (username, email))
+        if rc != 0:
+            d = {"result":"fail", "message": "username or email exist"}
             return jsonify(d)
-        d = {"result":"ok"}
+        cursor.execute("insert into account (username, pass, email, name_) values (%s, %s, %s,%s)", (username, password, email, name))
         conn.commit()
-        return jsonify(d)
+        return jsonify({"result":"ok"})
     except Exception as e:
-        print(e)
-    finally:
-        if conn.open:
-            cursor.close()
-            conn.close()
-            
-@app.route('/updateId', methods = ['GET', 'POST', 'PUT'])
-def updateId():
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        json_ = request.json
-        username = json_['username']
-        id = json_['id']
-        rc = cursor.execute("update account set id = %s where username = %s", (id , username))
-        if rc == 0:
-            d = {"result":"fail"}
-            return jsonify(d)
-        conn.commit()
-        d = {"result":"ok"}
-        return jsonify(d)
-    except Exception as e:
-        print(e)
+        return jsonify(e)
     finally:
         if conn.open:
             cursor.close()
             conn.close()
 
-@app.route('/getId', methods = ['GET', 'POST'])
-def getId():
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        json_ = request.json
-        username = json_['username']
-        rc = cursor.execute("select id from account where username = %s", username)
-        if rc == 0:
-            d = {"result":"fail", "message" : "no id find"}
-            return jsonify(d)
-        d = {"result":"ok", "message":"success", "id":cursor._result.rows[0][0]}
-        return d
-    except Exception as e:
-        print(e)
-    finally:
-        if conn.open:
-            cursor.close()
-            conn.close()
 if __name__ == '__main__':
     app.run()
